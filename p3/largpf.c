75b1b53e66558747770c5c216cb01ada81681b3f
ext4@Simple <thakurgithub@gmail.com>
2019-08-15 17:09:43 -0600

    after changing the long integer problem

--- p3/largpf.c
+++ p3/largpf.c
@@ -3,16 +3,16 @@
 int	main(void)
 {
 	int i;
-	int nb;
-	int subs;
+	long int nb;
+	long int subs;
 	int largestprime;
 
 	i = 1;
 	printf("Type the number to find its largest prime factor:\n");
-	scanf("%d", &nb);
+	scanf("%ld", &nb);
 
 	subs = nb;
-	printf("The number you have entered is %d\n", nb);
+	printf("The number you have entered is %ld\n", nb);
 	while (i <= nb)
 	{
 		if (nb % i == 0)
@@ -20,12 +20,12 @@ int	main(void)
 			while (nb % i == 0)
 			{
 				nb = nb / i;
-				printf("%d , %d\n", i, nb);
+				printf("%d, %ld\n", i, nb);
 				largestprime = i++;
 			}
 		}
 		i++;
 	}
-	printf("The largest prime number of %d number is %d\n",subs, largestprime);
+	printf("The largest prime number of %ld number is %d\n",subs, largestprime);
 	return (0);
 }
